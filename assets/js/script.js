0daf20725f0f970c2e333e46604d4a69fe33a7d8
Talee Y <tyang6@gustavus.edu>
2022-08-03 13:22:25 -0500

    Add the following changes to index.html and script.js:
    * Assign each time block a new class with a number value
    
    * Add new script to show current date on webpage
    
    * Add new script to color code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-var main = $('.container');
+var $main = $('.container');
+var $currentDate = $('#currentDay');
+
+$currentDate.text(moment().format('dddd, MMMM Do'));
+
+function initalize(){
+    var currentHour = moment().format('H');//military time
+    for(var i = 9; i < 18; i++){//modify the '18' if you want to add more timeblocks in the future
+        if(i < currentHour){
+            $('textarea.' + i).addClass('past');
+        }else if(i == currentHour){
+            $('textarea.' + i).addClass('present');
+        } else{
+            $('textarea.' + i).addClass('future');
+        }
+    }
+}
+
+setInterval(function(){//This function runs every minute
+    var currentHour = moment().format('H');//military time
+    for(var i = 9; i < 18; i++){//modify the '9' if you want to add more timeblocks in the future
+        if(i == currentHour){
+            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('present');
+        }else if(i < currentHour){
+            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('past');
+        } else{
+            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('future');
+        }
+    }
+}, 60000);
+
+initalize();
