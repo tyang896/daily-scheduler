2e3e28a701951c8d1a719dbede8ddee3f4976c34
Talee Y <tyang6@gustavus.edu>
2022-08-04 15:35:17 -0500

    Add new button icons and save function to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var $main = $('.container');
 var $currentDate = $('#currentDay');
+var $saveEvent = $('.saveBtn');
+var $saveIcon = $('i');
+
+//An array of objects holding all events
+var currentEvents = [];
 
 $currentDate.text(moment().format('dddd, MMMM Do'));
 
@@ -14,19 +19,59 @@ function initalize(){
             $('textarea.' + i).addClass('future');
         }
     }
+    if(localStorage.getItem('allEvents')){
+        var eventSaved = JSON.parse(localStorage.getItem('allEvents'));
+        currentEvents = eventSaved;
+        for(var i = 0; i < eventSaved.length; i++){
+            $('textarea.' + eventSaved[i].time).text(eventSaved[i].event);
+        }
+    }
 }
 
 setInterval(function(){//This function runs every minute
     var currentHour = moment().format('H');//military time
-    for(var i = 9; i < 18; i++){//modify the '9' if you want to add more timeblocks in the future
-        if(i == currentHour){
-            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('present');
-        }else if(i < currentHour){
-            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('past');
+    for(var i = 9; i < 18; i++){//modify the '18' if you want to add more timeblocks in the future
+        if(i < currentHour){
+            $('textarea.' + i).attr('class', "description col mb-1 past " + i );
+        }else if(i == currentHour){
+            $('textarea.' + i).attr('class', "description col mb-1 present " + i );
         } else{
-            $main.children('time-block').children('.row').children('textarea' + '.' + i).addClass('future');
+            $('textarea.' + i).attr('class', "description col mb-1 future " + i );
         }
     }
 }, 60000);
+
+$saveEvent.on('click', function(event){//This works for the button, but not for the tag that's  inside the button
+    event.preventDefault();
+    var $element = $(event.currentTarget);
+    var $eventInfo = $element.prev().val();
+    var $timeInfo = $element.data('time');
+    var newEvent = {
+        event: $eventInfo,
+        time: $timeInfo,
+    }
+
+    if(localStorage.getItem('allEvents')){//if allEvents exists in local storage
+        var eventSaved = JSON.parse(localStorage.getItem('allEvents'));
+        var eventExists = false;
+        for(var i = 0; i < eventSaved.length; i++){
+            if(newEvent.time === eventSaved[i].time){//Overwrite an old event
+                eventSaved[i] = newEvent;
+                currentEvents = eventSaved;
+                localStorage.setItem('allEvents', JSON.stringify(eventSaved));
+                eventExists = true;
+            }
+        }
+        if(!eventExists){//Adds a new event to local storage
+            eventSaved.push(newEvent);
+            currentEvents = eventSaved;
+            localStorage.setItem('allEvents', JSON.stringify(eventSaved));
+        }
+    }else{
+        currentEvents.push(newEvent);
+        localStorage.setItem('allEvents', JSON.stringify(currentEvents));
+    }
+
+})
 
 initalize();
